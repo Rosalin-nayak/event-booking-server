fb66bfed4c634ff40884956a53111116c5bd6f80
Rosalin-nayak <rosalinnayak063@gmail.com>
2025-08-07 14:50:35 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,13 @@
 const mongoose=require("mongoose");
 const connectDB=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL),{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            ssl:true,
+            tlsAllowInvalidCertificates:false,
+            tlsInsecure:false,
+        };
         console.log("MongoDB connected");
     }catch(error){
         console.error(error);
